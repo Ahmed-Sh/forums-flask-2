2a5a7244c53de30955cf61ade952c85bd0377025
Unknown <a.h.moslem@gmail.com>
2018-03-10 21:30:48 +0200

    Web API

--- app/models.py
+++ app/models.py
@@ -13,6 +13,13 @@ class member:
     def __repr__(self):
         return """Name: {} , Age: {}""".format(self.name, self.age)
 
+    def __dict__(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "posts": self.posts,
+        }
 
 class post:
     def __init__(self, title, content, member_id=0):
@@ -24,3 +31,11 @@ class post:
 
     def __repr__(self):
         return """Title: {} , Content: {}""".format(self.title, self.content)
+
+    def __dict__(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "member_id": self.member_id,
+        }
--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, jsonify
 from app import models
 from app import app, member_store, post_store
+
 
 @app.route("/")
 @app.route("/index")
 def home():
     return render_template("index.html", posts = post_store.get_all())
-
 
 @app.route("/topic/add", methods = ["GET", "POST"])
 def topic_add():
